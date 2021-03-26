1de2ee4a3f45f4e19e0754d2d85d587553ca16c3
Erfan <37825504+erfantkerfan@users.noreply.github.com>
2021-03-26 23:46:52 +0430

    end of chapter 2

--- first_app/views.py
+++ first_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
 
 # Create your views here.
+
+def signup_view(request):
+    return HttpResponse('Signup Completed!')
